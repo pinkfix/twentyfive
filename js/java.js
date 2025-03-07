0e0a4e64c394fff8a2e0951ebf3a09e76cee3c47
pinkfix <robin@willim.se>
2025-03-07 14:24:04 +0100

    Lagt till saker och annat har gått sönder
    Jag kommer inte att hinna och jag har inte tillräckligt med fritid för att göra det på fritiden

--- js/java.js
+++ js/java.js
@@ -1,11 +1,19 @@
-document.querySelector(".fade-layer").addEventListener("click", showMenu)
-document.querySelector(".menu-button").addEventListener("click", showMenu)
-
-
-function showMenu(){
-    let menu = document.querySelector("nav.menu");
-    menu.classList.toggle("show");
-
-    let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible");
-}
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
